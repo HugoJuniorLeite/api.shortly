261de6dde4f58a36586287dbb3848d2093631412
Hugo Junior <hjunioreng@gmail.com>
2023-02-25 09:22:19 -0300

    FIX: changed variable confirmpassword to confirmPassword for the evaluator

--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -4,5 +4,5 @@ export const signUpSchema = joi.object({
     name: joi.string().required(),
     email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'br'] } }).required(),
     password: joi.string().required(),
-    confirmpassword: joi.ref( 'password' )
+    confirmPassword: joi.ref( 'password' )
 })
